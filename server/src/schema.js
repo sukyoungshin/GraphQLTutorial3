a3dac766b6e03b9b68f6b847106ddff9709e5bd8
SuKyoung Shin <sukyoung.dev@gmail.com>
2022-07-15 14:14:02 +0900

    feat: Track 요청하는 query 및 schema 수정

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,7 +4,8 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-    track: Track
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
